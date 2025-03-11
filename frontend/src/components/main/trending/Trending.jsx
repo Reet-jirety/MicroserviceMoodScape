d9f84f701be809dd47c5d84bf588d2db0e19c61e
Pallav46 <pallavkumar6200@gmail.com>
2025-03-11 09:48:55 +0530

    MoodScape: Frontend => Minor bug fixed.

--- frontend/src/components/main/trending/Trending.jsx
+++ frontend/src/components/main/trending/Trending.jsx
@@ -6,8 +6,8 @@ export const Trending = () => {
       <div className="left">
         <h5>Trending New Song</h5>
         <div className="info mt-[12px] p-[26px]">
-          <h2 className='text-[56px] mb-[8px] max-4xl:text-[36px] max-3xl:text-[24px]'>Lost Emotions</h2>
-          <h4 className='inline'>Rion Clarke</h4>
+          <h2 className='text-[56px] mb-[8px] max-4xl:text-[36px] max-3xl:text-[24px] font-bold text-white'>Lost Emotions</h2>
+          <h4 className='inline text-white font-bold'>Rion Clarke</h4>
           <h5 className='inline ml-[12px] text-cool-gray-txt max-3xl:block max-3xl:text-[12px] max-3xl:ml-0'>63 Million Plays</h5>
           <div className="buttons mt-[30px] flex items-center gap-[16px]">
             <button className='py-[12px] px-[16px] bg-blue-foreground border-none rounded-[14px] text-secondary font-bold cursor-pointer max-3xl:py-[10px] max-3xl:px-[12px] max-3xl:text-[12px]'>Listen Now</button>
