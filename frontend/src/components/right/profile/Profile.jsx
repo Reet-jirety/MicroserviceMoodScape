65c4cfdde2ba047181ee99ecb81957181bed1a80
Pallav46 <pallavkumar6200@gmail.com>
2025-03-12 15:29:53 +0530

    Bug Fix

--- frontend/src/components/right/profile/Profile.jsx
+++ frontend/src/components/right/profile/Profile.jsx
@@ -1,5 +1,5 @@
-// import React from "react";
-import{ SignedOut, SignedIn } from "@clerk/clerk-react";
+import React from "react";
+import { SignedOut, SignedIn } from "@clerk/clerk-react";
 import SignInOAuthButtons from "./SignInOAuthButtons";
 import { SignOutButton } from "@clerk/clerk-react";
 
@@ -9,15 +9,16 @@ export const Profile = () => {
       <i className="bx bxs-bell text-white"></i>
       <i className="bx bxs-cog text-white"></i>
       <div className="user flex">
-       
-      <SignedIn> 
-        <SignOutButton />
-      </SignedIn>
+        <SignedIn>
+          <div className="text-white">
+            <SignOutButton />
+          </div>
+        </SignedIn>
 
-      <SignedOut>
-         <SignInOAuthButtons />
-      </SignedOut>
-    </div>
+        <SignedOut>
+          <SignInOAuthButtons />
+        </SignedOut>
+      </div>
     </div>
   );
 };
